cca88d1d90fbf2188c4cefe07a7eff127abd6f1e
naangarchive <alsrlskd@gmail.com>
2026-04-30 04:16:19 +0900

    chore: PWA 서비스 워커 등록 및 타입 참조 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+    console.log("새 버전이 있습니다.");
+  },
+  onOfflineReady() {
+    console.log("오프라인 사용 준비 완료");
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
