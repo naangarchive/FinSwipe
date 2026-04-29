0a114aa8b0597e8209dba526e15178fd14f5fe47
naangarchive <alsrlskd@gmail.com>
2026-04-30 04:32:08 +0900

    chore: Capacitor 설정 및 Android 빌드 환경 구성

---
+++ android/app/src/main/java/com/finswipe/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.finswipe.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.finswipe.app',
+  appName: 'finswipe',
+  webDir: 'dist'
+};
+
+export default config;
