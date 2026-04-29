cc48221269c4d0dbd8abcb796de3e46c2e06c300
naangarchive <alsrlskd@gmail.com>
2026-04-30 04:07:17 +0900

    chore: PWA 설정을 위한 패키지 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'FinSwipe',
+        short_name: 'FinSwipe',
+        description: 'FinSwipe PWA App',
+        theme_color: '#0064FF',
+        background_color: "#ffffff",
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+        ]
+      },
+    }),
+  ],
 })
